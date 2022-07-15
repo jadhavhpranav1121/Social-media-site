a4626a40231245ed18a9dfcec64db2ca9dc53e69
Pranav Jadhav <jadhavhpranav1121@gmail.com>
2022-07-15 10:47:37 +0530

    Adding bulk of  changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    
+    apiKey: "AIzaSyCR4GgkJHZJy6aNVH55deBDNe7BV769Klg",
+    authDomain: "social-media-8fc2b.firebaseapp.com",
+    projectId: "social-media-8fc2b",
+    storageBucket: "social-media-8fc2b.appspot.com",
+    messagingSenderId: "73777798896",
+    appId: "1:73777798896:web:7de9e4101b1555c6ce6c35",
+    measurementId: "G-YBYP56DTF4"
+}
+};
